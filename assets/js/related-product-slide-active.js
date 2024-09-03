6d0fdb923dad5fb5ce3e19dd0c9356ba19706f0e
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2024-09-03 22:44:19 +0600

    product page and login page

--- assets/js/related-product-slide-active.js
+++ assets/js/related-product-slide-active.js
@@ -1,14 +1,15 @@
 
-/*------------------------------------
-     gallery  Slider - index.html 
-   --------------------------------------*/
 $(document).ready(function () {
 
-    const relatedSwiperContainer = document.querySelector('.related-product-active .swiper-container')
+    /*------------------------------------
+         featured products Slider - index.html 
+       --------------------------------------*/
+
+    const featureSwiperContainer = document.querySelector('.related-product-active.feature-product .swiper-container')
 
     // Slider With Thumbs
-    if (jQuery(relatedSwiperContainer).length > 0) {
-        let destinationSlider = new Swiper(relatedSwiperContainer, {
+    if (jQuery(featureSwiperContainer).length > 0) {
+        let destinationSlider = new Swiper(featureSwiperContainer, {
             // Optional parameters
             slidesPerView: 4,
             slidesPerColumn: 1,
@@ -38,14 +39,63 @@ $(document).ready(function () {
         });
     }
 
-    const galleryBtnPrev = document.querySelector('.related-product-button .btn-prev');
-    const galleryBtnNext = document.querySelector('.related-product-button .btn-next');
+    const featureBtnPrev = document.querySelector('.related-product-active.feature-product .related-product-button .btn-prev');
+    const featureBtnNext = document.querySelector('.related-product-active.feature-product .related-product-button .btn-next');
 
-    galleryBtnPrev.addEventListener('click', () => {
-        gallerySwiperContainer.swiper.slidePrev();
+    featureBtnPrev.addEventListener('click', () => {
+        featureSwiperContainer.swiper.slidePrev();
     });
 
-    galleryBtnNext.addEventListener('click', () => {
-        gallerySwiperContainer.swiper.slideNext();
+    featureBtnNext.addEventListener('click', () => {
+        featureSwiperContainer.swiper.slideNext();
+    })
+
+    /*------------------------------------
+         new arrivals product  Slider - index.html 
+       --------------------------------------*/
+
+    const newSwiperContainer = document.querySelector('.related-product-active.new-product .swiper-container')
+
+    // Slider With Thumbs
+    if (jQuery(newSwiperContainer).length > 0) {
+        let destinationSlider = new Swiper(newSwiperContainer, {
+            // Optional parameters
+            slidesPerView: 4,
+            slidesPerColumn: 1,
+            loop: true,
+            spaceBetween: 24,
+            autoplay: {
+                delay: 5000,
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+                1200: {
+                    slidesPerView: 4,
+                },
+
+            },
+
+            a11y: false
+
+        });
+    }
+
+    const newBtnPrev = document.querySelector('.related-product-active.new-product .related-product-button .btn-prev');
+    const newBtnNext = document.querySelector('.related-product-active.new-product .related-product-button .btn-next');
+
+    newBtnPrev.addEventListener('click', () => {
+        newSwiperContainer.swiper.slidePrev();
+    });
+
+    newBtnNext.addEventListener('click', () => {
+        newSwiperContainer.swiper.slideNext();
     })
 });
